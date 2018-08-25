2507b4ecfeacbb008827bdd37f2d93b42751a06f
Murali <muralidharan@touch2success.com>
2018-08-25 19:20:01 +0530

    Updated the node modules

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+	database: {
+		host:	  'localhost', 	// database host
+		user: 	  'homestead', 		// your database username
+		password: 'secret', 		// your database password
+		port: 	  33060, 		// default MySQL port
+		db: 	  'foxy_project56' 		// your database name
+	}
+}
+
+module.exports = config

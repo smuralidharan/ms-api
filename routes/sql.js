5583e21a03059d556c39665b1d3021b6af5af231
Murali <muralidharan@touch2success.com>
2018-08-29 17:52:33 +0530

    Updated the db credentials

--- routes/sql.js
+++ routes/sql.js
@@ -1,8 +1,8 @@
 var mysql      = require('mysql');
 var msDb = mysql.createConnection({
-	host     : 'localhost',
-	user     : 'homestead',
-	password : 'secret',
+	host     : '52.208.53.100',
+	user     : 'ssostag',
+	password : 'sskDwm8X',
 	port: 33060,
 	database : 'foxy_project56'
 });

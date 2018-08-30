f06e5cc2407552a45c95a143b5f620a757b6418f
Murali <muralidharan@touch2success.com>
2018-08-30 08:52:02 +0530

    updated the sql

--- routes/sql.js
+++ routes/sql.js
@@ -1,6 +1,6 @@
 var mysql      = require('mysql');
 var msDb = mysql.createConnection({
-	host     : 'localhost',
+	host     : '52.208.53.100',
 	user     : 'ssostag',
 	password : 'sskDwm8X',
 	port: 3306,

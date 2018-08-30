a52bd314c1ec403535ab752a5a6020be332d0c7d
Murali <muralidharan@touch2success.com>
2018-08-30 17:53:51 +0530

    updated the sql

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var config = {
 	database: {
 		host:	  'localhost', 	// database host
-		user: 	  'homestead', 		// your database username
-		password: 'secret', 		// your database password
-		port: 	  33060, 		// default MySQL port
+		user: 	  'ssostag', 		// your database username
+		password: 'sskDwm8X', 		// your database password
+		port: 	  3306, 		// default MySQL port
 		db: 	  'foxy_project56' 		// your database name
 	}
 }
